4c3074a0277df5b430c87cf2dc85a13d92394015
Courtney Green <courtg47@gmail.com>
2016-11-12 17:47:06 -0600

    adding JS file

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function popup1() {
+	alert("See All Features");
+}
+
+function popup2() {
+	alert("Learn More About Us");
+}
+
+function popup3() {
+	alert("Visit Our Store");
+}
+
+function popup4(){
+	alert("Visit Our Blog Now");
+}
+
+function popup5() {
+	alert("Learn More Now");
+}
+
+function popup6() {
+	alert("James Andrews on CBS News");
+}
+
+function popup7() {
+	alert("Jersey Watch Network");
+}
+
+function popup8() {
+	alert("Tommy John Surgery");
+}
+
